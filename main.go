ad6d0489287421f6ebbce9e13baf6b723842a35a
Nicolás Joaquín Parisi <78542476+NicolasJoaquinParisi@users.noreply.github.com>
2023-09-06 23:15:07 -0300

    fix: sintax error in album struct type definition

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 )
 
 type album struct {
-	ID     string  `json: "id"`
-	Title  string  `json: "title"`
-	Artist string  `json: "artist`
-	Price  float64 `json: "price"`
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
 }
 
 var albums = []album{
